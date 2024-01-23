037b5a21e9ceaeb09fc714ba1ef9d927e84cff16
Sergio_Pavon <pavon_madrid999@hotmail.com>
2024-01-23 18:40:09 +0100

    18:40 23/01

---
+++ _05_PringBoot/src/main/java/com/uem/psp/entidad/copy/Persona.java
@@ -0,0 +1,51 @@
+package com.uem.psp.entidad.copy;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+// Con la anotacion @component se creara un objeto de tipo persona (solo uno) 
+//en el contexto de Spring, cuyo nombre será "persona".
+//Su ciclo de vida, no lo lleva el programador, lo lleva Spring -->Inversion de Control
+@Component
+@Entity
+public class Persona {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String nombre;
+	private int id;
+	private int edad;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", id=" + id + ", edad=" + edad + "]";
+	}
+
+}
